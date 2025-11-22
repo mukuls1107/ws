bb2932ec2d44ae714f76293db9337e32276f7bb5
mukuls1107 <mukulsingh3078@gmail.com>
2025-11-22 11:05:28 +0530

    akudwk

--- dist/index.js
+++ dist/index.js
@@ -5,9 +5,7 @@ const wss = new ws_1.WebSocketServer({ port: 8080 });
 wss.on("connection", (socket) => {
     console.log("New client connected");
     socket.on("message", (msg) => {
-        if (msg.toString() == "ping") {
-            socket.send(`pong!`);
-        }
+        socket.send(`${new Date().getUTCHours()} :${msg.toString()}`);
         // socket.send(`Received: ${msg.toString()}`)
         console.log(msg.toString());
     });
--- src/index.ts
+++ src/index.ts
@@ -9,9 +9,7 @@ wss.on("connection", (socket)=>{
 
 
     socket.on("message", (msg)=>{
-        if(msg.toString() == "ping" ){
-            socket.send(`pong!`)
-        }
+       socket.send(`${new Date().getUTCHours()} :${msg.toString()}`)
         // socket.send(`Received: ${msg.toString()}`)
 
         console.log(msg.toString())
